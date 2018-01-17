a47f4df7d2d703fcf0d759fbab6d0872eaeabd28
HuangQiii <598260403@qq.com>
2018-01-17 21:58:39 +0800

    [ADD] 第一版DigitalPlatform通用请求

--- app/utils/RequestUtil.js
+++ app/utils/RequestUtil.js
@@ -1,7 +1,7 @@
 const request = (url, token, method = 'get', body) => {
     let header = {
         "Content-Type": "application/json;charset=UTF-8",
-        "accesstoken": token
+        "Authorization": token
     };
     return new Promise((resolve, reject) => {
         fetch(url, {
@@ -11,7 +11,9 @@ const request = (url, token, method = 'get', body) => {
         })
             .then((response) => response.json())
             .then((responseData) => {
-                if (responseData.error === undefined) {
+                if (responseData.error === 'invalid_token') {
+                    console.log('invalid_token,should back to Login');
+                } else if (responseData.error === undefined) {
                     resolve(responseData);
                 } else {
                     reject(responseData);
