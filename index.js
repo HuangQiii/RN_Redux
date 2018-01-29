fb737d5d109b0640e497929fef734f8d5591cbb2
HuangQiii <598260403@qq.com>
2018-01-29 20:45:55 +0800

    [IMP] 完全迁移到0.52.2

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 import { AppRegistry } from 'react-native';
 import Root from './app/Root';
 
-AppRegistry.registerComponent('cloud_main_app', () => Root);
+AppRegistry.registerComponent('RN_Redux', () => Root);
